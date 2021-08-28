fc1680188dc9874688fd9a35b67e69de1e6207ee
GSergey <example@gmail.com>
2021-08-28 19:27:16 +0300

    init: expression-calculator

--- src/index.js
+++ src/index.js
@@ -4,8 +4,60 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let equation = expr.split(" ");
+
+    function calcPlusMinus(eq) {
+        let calc = 0;
+        for (let i = 0; i < eq.length ; i++) {
+            if (eq[i] === "+") {
+                calc = Number(eq[i - 1]) + Number(eq[i + 1])
+                eq.splice(i - 1 , 3, calc);
+                i--;
+                }
+            if (eq[i] === "-") {
+                calc = Number(eq[i - 1]) - Number(eq[i + 1])
+                eq.splice(i - 1 , 3, calc);
+                i--;
+                }
+            }
+        return eq[0];
+    }
+
+    function calcDivisionMultiplication(equation) {
+        let x;
+        for (let i = 0; i < equation.length ; i++) {
+            if (equation[i] === "") {equation.splice(i,1)};
+            if (equation[i] === "*" && equation[i + 1] !== "(" && equation[i - 1] !== ")") {
+                x = equation[i - 1] * equation[i + 1];
+                equation.splice(i - 1, 3, x);
+                i--;
+                }
+            if (equation[i] === "/" && equation[i + 1] !== "(" && equation[i - 1] !== ")") {
+                x = equation[i - 1] / equation[i + 1];
+                equation.splice(i - 1, 3, x);
+                i--;
+                }
+        }
+        return equation;
+    }
+
+    equation = calcDivisionMultiplication(equation);
+
+    while (equation.lastIndexOf("(") !== -1) {
+        let splitedEquation = []
+        let splitedEquationStart = equation.lastIndexOf("(");
+        let splitedEquationEnd = equation.indexOf(")");
+        splitedEquation = equation.slice(splitedEquationStart + 1, splitedEquationEnd);
+        let lengthSplitedEquation = splitedEquation.length + 2;
+        splitedEquation = calcDivisionMultiplication(splitedEquation);
+        splitedEquation = calcPlusMinus(splitedEquation);
+        equation.splice(splitedEquationStart, lengthSplitedEquation, splitedEquation);
+    }
+
+    equation = calcPlusMinus(calcDivisionMultiplication(equation));
+    return Number(equation.toFixed(4));
 }
+
 
 module.exports = {
     expressionCalculator
